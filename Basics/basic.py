b96d9a058dc48bf3d6261da94f4b557d7270633c
Raja Prasad <prasadr731@gmail.com>
2024-03-19 11:07:08 +0530

    uploading basic folder

---
+++ Basics/basic.py
@@ -0,0 +1,28 @@
+# First Code
+num = ("enter a number")
+print(num)
+
+print() # printing a empty line
+
+num = input("enter a number: ")
+print(num)
+
+print(type(num)) # printing the type of (num)
+print() # printing a empty line
+
+# TypeCasting
+
+print("Type casting from String to Integer")
+num = int(input("enter a number: "))
+print(num)
+
+print(type(num)) # printing the type of (num)
+
+print() # printing a empty line
+
+print("TypeCasting from Str to Float")
+num = float(input("enter a number: "))
+print(num)
+
+print(type(num)) # printing the type of (num)
+
